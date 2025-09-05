7cfe46da0ad694ae62784f4e5cdcaa93e6e89483
Vachek-hooves <vachek0602@gmail.com>
2025-09-05 14:14:49 +0300

    realese on test

---
+++ config/credentials.js
@@ -0,0 +1,29 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'pCY43sbDhDN9SBrRP7iQf9',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - c02746b92a18761f921e2ddfe2313b95
+
+// FB app cred
+export const FB_APP_ID = '1457354268934977';
+export const FB_CLIENT_TOKEN = 'a8882a993a742ca69dd73851b6348b7b';
+
+// Compaign
+// https://drift-lake-garden.space/OhYjmq6d
+export const INITIAL_URL = `https://drift-lake-garden.space/`;
+export const URL_IDENTIFAIRE = `OhYjmq6d`;
+
+// one signal key ex 3553 Holland Tulip Quiz
+export const ONE_SIGNAL_ID = '26a1faf1-1f45-42f4-bb8e-676391e87863';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - 42a82801-2171-4193-b134-a6c361bbb6d2
